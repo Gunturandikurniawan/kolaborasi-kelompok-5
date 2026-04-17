ae45ed74a1bb6c6212ef545a17efcad59d9f062e
Ridho Khatami <printridho123@gmail.com>
2026-04-17 10:27:52 +0700

    Penambahan fitur login di guntur/main.py

--- guntur/main.py
+++ guntur/main.py
@@ -1,1 +1,28 @@
-print ("baru")
+username = input("Isi username : ")
+password = input("Isi password : ")
+
+data = [
+    {
+        "username" : "guntur123",
+        "password" : "123"
+    },
+    
+    {
+        "username" : "fadel123",
+        "password" : "123"
+    }
+    
+]
+
+bool = False
+
+for i in data :
+    if (i["username"] == username) & (i["password"] == password) :
+        bool = True
+        print("Akun ada")
+        break
+
+if not bool :
+    print("Akun tidak ada")
+        
+    
